59b32df2e67e7f6627543a3bd382f26566e148b8
vinnylwoods <vinnylwoods@gmail.com>
2014-01-07 21:27:34 +1100

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,18 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact Page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+    end
+  end
+
 end
